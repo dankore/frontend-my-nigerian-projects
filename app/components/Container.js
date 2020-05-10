c40edf6219198072c73e8dd4ecf97ffdeae8aab6
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-10 02:15:42 -0500

    css and html

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 
 function Container(props) {
-  return <div className={"max-w-3xl py-5 " + (props.wide ? "" : "max-w-2xl")}>{props.children}</div>;
+  console.log(props);
+  return <div className={`mx-auto mt-10 ${props.wide ? 'max-w-3xl' : 'max-w-2xl'}`}>{props.children}</div>;
 }
 
 export default Container;
--- app/components/Footer.js
+++ app/components/Footer.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <div>
+    <div className="text-center mt-10">
       <div>
         <Link to="/">Home</Link> <Link to="about">About</Link> <Link to="/terms">Terms</Link>
       </div>
