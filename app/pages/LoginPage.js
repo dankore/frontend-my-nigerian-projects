e2ff8aede1cac1ac4bc332f7909e159c4e802620
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-10 16:07:10 -0500

    registration page: autoComplete values

--- app/pages/LoginPage.js
+++ app/pages/LoginPage.js
@@ -5,13 +5,13 @@ import { Link } from "react-router-dom";
 function LoginPage() {
   return (
     <Page title="Login">
-      <div className="px-2 mx-auto">
+      <div className="px-2 max-w-sm mx-auto">
         <form className="p-3 sm:p-4 border rounded">
           <div className="mb-4">
             <label htmlFor="email" className="w-full text-xs font-bold block mb-1 uppercase tracking-wide text-gray-700 ">
               Enter Your Email
             </label>
-            <input id="email" name="email" type="text" className="w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight" />
+            <input id="email" type="text" autoComplete="email" className="w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight" />
           </div>
           <div className="mb-4">
             <div className="flex justify-between mb-1 text-xs uppercase font-bold tracking-wide text-gray-700">
@@ -19,7 +19,7 @@ function LoginPage() {
                 Enter Your Password{" "}
               </label>
             </div>
-            <input id="password" name="password" type="password" autoComplete="current-password" className="w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight" />
+            <input id="password" type="password" autoComplete="current-password" className="w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight" />
           </div>
           <button className="w-full text-white rounded border border-white bg-blue-600 hover:bg-blue-800 px-2 py-3">Login</button>
         </form>
--- app/pages/RegistrationPage.js
+++ app/pages/RegistrationPage.js
@@ -5,32 +5,32 @@ import Page from "../components/Page";
 function RegistrationPage() {
   return (
     <Page title="Registration">
-      <div className="flex justify-center mx-auto">
+      <div className="flex justify-center max-w-sm mx-auto">
         <form className="mx-auto p-3 border rounded bg-white">
           <div className="flex flex-wrap -mx-3 mt-1">
             <div className="relative w-full px-3 mb-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1" htmlFor="username">
                 Username <span className="text-red-600">*</span>
               </label>
-              <input id="username" autoComplete="off" spellCheck="false" className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text" />
+              <input id="username" autoComplete="username" spellCheck="false" className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text" />
             </div>
             <div className="relative w-full px-3 mb-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1" htmlFor="first-name">
                 First Name <span className="text-red-600">*</span>
               </label>
-              <input id="first-name" autoComplete="off" spellCheck="false" className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text" />
+              <input id="first-name" autoComplete="given-name" spellCheck="false" className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="first-name" type="text" />
             </div>
             <div className="relative w-full px-3 mb-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1" htmlFor="last-name">
                 Last Name <span className="text-red-600">*</span>
               </label>
-              <input id="last-name" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="last-name" type="text" />
+              <input id="last-name" autoComplete="family-name"  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="last-name" type="text" />
             </div>
             <div className="relative w-full px-3 mb-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1" htmlFor="email">
                 Email <span className="text-red-600">*</span>
               </label>
-              <input id="email" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="text" />
+              <input id="email" autoComplete="email" className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="text" />
             </div>
             <div className="relative w-full px-3">
               <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1" htmlFor="password">
@@ -43,7 +43,7 @@ function RegistrationPage() {
             <div className="text-xs flex justify-center w-full">
               <p>By clicking Create Account, you agree to the</p>
               <Link to="/terms" className="text-blue-600 ml-1" rel="nofollow">
-                Terms and Conditions
+                Terms of Use
               </Link>
             </div>
           </div>
