838b7c912ef32c5b47733b3920919fc7c8c5ab52
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-12 00:40:20 -0500

    css and more

--- app/pages/RegistrationPage.js
+++ app/pages/RegistrationPage.js
@@ -70,7 +70,6 @@ function RegistrationPage() {
         }
         return;
       case 'usernameIsUnique':
-        console.log(action.value)
         if (action.value) {
           draft.username.hasErrors = true;
           draft.username.isUnique = false;
@@ -79,15 +78,11 @@ function RegistrationPage() {
           draft.username.isUnique = true;
         }
         return;
-      // USERNAME ENDS
       // FIRST NAME
       case 'firstnameImmediately':
         draft.firstName.hasErrors = false;
         draft.firstName.value = action.value;
         return;
-      case 'firstnameAfterDelay':
-        return;
-      // FIRST NAME ENDS
       // LAST NAME
       case 'lastnameImmediately':
         draft.lastName.hasErrors = false;
@@ -95,25 +90,45 @@ function RegistrationPage() {
         return;
       case 'lastnameAfterDelay':
         return;
-      // LAST NAME ENDS
       // EMAIL
       case 'emailImmediately':
         draft.email.hasErrors = false;
         draft.email.value = action.value;
         return;
       case 'emailAfterDelay':
+        if (!/^\S+@\S+$/.test(draft.email.value)) {
+          draft.email.hasErrors = true;
+          draft.email.message = 'Please provide a valid email.';
+        }
+        if (!draft.email.hasErrors) {
+          draft.email.checkCount++;
+        }
         return;
       case 'emailIsUnique':
+        if (action.value) {
+          draft.email.hasErrors = true;
+          draft.email.isUnique = false;
+          draft.email.message = 'That email is already being used.';
+        } else {
+          draft.email.isUnique = true;
+        }
         return;
-      // EMAIL ENDS
       // PASSWORD
       case 'passwordImmediately':
         draft.password.hasErrors = false;
         draft.password.value = action.value;
+
+        if (draft.password.value.length > 50) {
+          draft.password.hasErrors = true;
+          draft.password.message = 'Password must cannot be more than 50 characters.';
+        }
         return;
       case 'passwordAfterDelay':
+        if (draft.password.value.length < 7) {
+          draft.password.hasErrors = true;
+          draft.password.message = 'Password must be at least 12 characters.';
+        }
         return;
-      // PASSWORD ENDS
       // SUBMIT
       case 'submitForm':
         return;
@@ -122,7 +137,7 @@ function RegistrationPage() {
 
   const [state, dispatch] = useImmerReducer(Reducer, initialState);
 
-  // DELAY
+  // DELAY: USERNAME
   useEffect(() => {
     if (state.username.value) {
       const delay = setTimeout(() => dispatch({ type: 'usernameAfterDelay' }), 800);
@@ -130,8 +145,44 @@ function RegistrationPage() {
       return () => clearTimeout(delay);
     }
   }, [state.username.value]);
+  // DELAY: EMAIL
+  useEffect(() => {
+    if (state.email.value) {
+      const delay = setTimeout(() => dispatch({ type: 'emailAfterDelay' }), 800);
 
-  // USERNAME
+      return () => clearTimeout(delay);
+    }
+  }, [state.email.value]);
+
+  // PASSWORD MINIMUM LENGTH
+  useEffect(() => {
+    if (state.password.value) {
+      const delay = setTimeout(() => dispatch({ type: 'passwordAfterDelay' }), 800);
+
+      return () => clearTimeout(delay);
+    }
+  }, [state.password.value]);
+
+  // EMAIL IS UNIQUE
+  useEffect(() => {
+    if (state.email.checkCount) {
+      const request = Axios.CancelToken.source();
+      (async function checkForEmail() {
+        try {
+          const response = await Axios.post('/doesEmailExist', { email: state.email.value }, { cancelToken: request.token });
+          dispatch({ type: 'emailIsUnique', value: response.data });
+          console.log(response.data);
+        } catch (error) {
+          alert('Having difficulty looking for your email. Please try again.');
+        }
+      })();
+      return function cleanUpRequest() {
+        return request.cancel();
+      };
+    }
+  }, [state.email.checkCount]);
+
+  // USERNAME IS UNIQUE
   useEffect(() => {
     if (state.username.checkCount) {
       const request = Axios.CancelToken.source();
@@ -153,6 +204,9 @@ function RegistrationPage() {
     e.preventDefault();
   }
 
+  // CSS
+  const CSSTransitionStyle = {color: '#e53e3e', fontSize: 0.75 + 'em'};
+
   return (
     <Page title='Registration'>
       <div className='flex justify-center max-w-sm mx-auto'>
@@ -164,7 +218,7 @@ function RegistrationPage() {
               </label>
               <input onChange={e => dispatch({ type: 'usernameImmediately', value: e.target.value })} id='username' autoComplete='off' spellCheck='false' className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='first-name' type='text' />
               <CSSTransition in={state.username.hasErrors} timeout={330} className='liveValidateMessage' unmountOnExit>
-                <div style={{ color: 'red', borderRadius: 0.25 + 'em', borderColor: '#fc8181', padding: 0.75 + 'em' }} className='liveValidateMessage'>
+                <div style={CSSTransitionStyle} className='liveValidateMessage'>
                   {state.username.message}
                 </div>
               </CSSTransition>
@@ -174,18 +228,33 @@ function RegistrationPage() {
                 First Name <span className='text-red-600'>*</span>
               </label>
               <input onChange={e => dispatch({ type: 'firstnameImmediately', value: e.target.value })} id='first-name' autoComplete='off' spellCheck='false' className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='first-name' type='text' />
+              <CSSTransition in={state.firstName.hasErrors} timeout={330} className='liveValidateMessage' unmountOnExit>
+                <div style={CSSTransitionStyle} className='liveValidateMessage'>
+                  {state.firstName.message}
+                </div>
+              </CSSTransition>
             </div>
             <div className='relative w-full px-3 mb-3'>
               <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1' htmlFor='last-name'>
                 Last Name <span className='text-red-600'>*</span>
               </label>
               <input onChange={e => dispatch({ type: 'lastnameImmediately', value: e.target.value })} id='last-name' autoComplete='off' className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500' id='last-name' type='text' />
+              <CSSTransition in={state.lastName.hasErrors} timeout={330} className='liveValidateMessage' unmountOnExit>
+                <div style={CSSTransitionStyle} className='liveValidateMessage'>
+                  {state.lastName.message}
+                </div>
+              </CSSTransition>
             </div>
             <div className='relative w-full px-3 mb-3'>
               <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1' htmlFor='email'>
                 Email <span className='text-red-600'>*</span>
               </label>
               <input onChange={e => dispatch({ type: 'emailImmediately', value: e.target.value })} id='email' autoComplete='off' className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500' id='email' type='text' />
+              <CSSTransition in={state.email.hasErrors} timeout={330} className='liveValidateMessage' unmountOnExit>
+                <div style={CSSTransitionStyle} className='liveValidateMessage'>
+                  {state.email.message}
+                </div>
+              </CSSTransition>
             </div>
             <div className='relative w-full px-3'>
               <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1' htmlFor='password'>
@@ -193,6 +262,11 @@ function RegistrationPage() {
               </label>
               <input onChange={e => dispatch({ type: 'passwordImmediately', value: e.target.value })} id='password' className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500' id='password' type='password' autoComplete='new-password' />
               <p className='text-red-300 text-xs mt-1 italic'>Password should be a minimum of 6 characters</p>
+              <CSSTransition in={state.password.hasErrors} timeout={330} className='liveValidateMessage' unmountOnExit>
+                <div style={CSSTransitionStyle} className='liveValidateMessage'>
+                  {state.password.message}
+                </div>
+              </CSSTransition>
             </div>
             <button className='rounded w-full bg-blue-600 hover:bg-blue-800 text-white m-3 p-3'>Create Account</button>
             <div className='text-xs flex justify-center w-full'>
