039a5b3a41e222fbc59a117bf0f193d5e61172c7
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-12 18:58:29 -0500

    profile set up

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -11,7 +11,7 @@ function HeaderLoggedIn() {
   return (
     <div className='flex justify-between items-center'>
       <Link data-for='profile' data-tip='Profile' to={`/profile/${appState.user.username}`} data-tip='My Profile' className='mr-2'>
-        <img className='w-8 h-8 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' />
+        <img className='h-10 w-10 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' alt='' />
       </Link>
       <ReactToolTip place='bottom' id='profile' />
       <Link className='bg-green-600 hover:bg-green-800 mr-2 rounded px-2' to='/create-bid'>
--- app/pages/ProfilePage.js
+++ app/pages/ProfilePage.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react"
-import Page from "../components/Page"
+import React, { useContext } from 'react';
+import Page from '../components/Page';
 import StateContext from '../StateContext';
 import { Link } from 'react-router-dom';
 
@@ -10,42 +10,80 @@ function ProfilePage() {
     <Page title='Profile Page'>
       <h2 className='flex items-center'>
         <Link to={`/profile/${appState.user.username}`} className='mr-2'>
-          <img className='w-8 h-8 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' />
+          <img className='h-10 w-10 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' alt='' />
         </Link>
         <Link to={`/profile/${appState.user.username}`}>Adamu</Link>
       </h2>
 
-      <ul class='flex border-b mt-5'>
-        <li class='-mb-px mr-1'>
-          <a class='bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold' href='#'>
+      <ul className='flex border-b mt-5'>
+        <li className='-mb-px mr-1'>
+          <a className='bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold' href='#'>
             Active
           </a>
         </li>
-        <li class='mr-1'>
-          <a class='bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold' href='#'>
+        <li className='mr-1'>
+          <a className='bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold' href='#'>
             Tab
           </a>
         </li>
-        <li class='mr-1'>
-          <a class='bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold' href='#'>
+        <li className='mr-1'>
+          <a className='bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold' href='#'>
             Tab
           </a>
         </li>
-        <li class='mr-1'>
-          <a class='bg-white inline-block py-2 px-4 text-gray-400 font-semibold' href='#'>
+        <li className='mr-1'>
+          <a className='bg-white inline-block py-2 px-4 text-gray-400 font-semibold' href='#'>
             Tab
           </a>
         </li>
       </ul>
-      <div className='list-group'>
-        <Link to='#' className='list-group-item list-group-item-action'>
-          <img className='w-8 h-8 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' />
-          <strong>Example Post #1</strong>
-  <span className='text-muted small'>{" "}on 2/10/2020 </span>
-        </Link>
+      <div className='flex flex-col'>
+        <div className='-my-2 py-2  sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8'>
+          <div className='align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200'>
+            <table className='min-w-full'>
+              <thead>
+                <tr>
+                  <th className='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>Name</th>
+                  <th className='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>Title</th>
+                  <th className='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>Status</th>
+                  <th className='px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider'>Role</th>
+                  <th className='px-6 py-3 border-b border-gray-200 bg-gray-50'></th>
+                </tr>
+              </thead>
+              <tbody className='bg-white'>
+                <tr>
+                  <td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
+                    <div className='flex items-center'>
+                      <div className='flex-shrink-0 h-10 w-10'>
+                        <img className='h-10 w-10 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' alt='' />
+                      </div>
+                      <div className='ml-4'>
+                        <div className='text-sm leading-5 font-medium text-gray-900'>Adamu M. Dankore</div>
+                        <div className='text-sm leading-5 text-gray-500'>bernardlane@example.com</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
+                    <div className='text-sm leading-5 text-gray-900'>Director</div>
+                    <div className='text-sm leading-5 text-gray-500'>Human Resources</div>
+                  </td>
+                  <td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
+                    <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>Active</span>
+                  </td>
+                  <td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500'>Owner</td>
+                  <td className='px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium'>
+                    <Link to='#' className='text-indigo-600 hover:text-indigo-900'>
+                      View
+                    </Link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </Page>
   );
 }
 
-export default ProfilePage
+export default ProfilePage;
