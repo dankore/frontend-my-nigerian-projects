52aa20f115fd81ed65c8f9094da19453d6c5ae09
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-12 18:22:11 -0500

    profile set up

--- app/pages/ProfilePage.js
+++ app/pages/ProfilePage.js
@@ -8,38 +8,41 @@ function ProfilePage() {
 
   return (
     <Page title='Profile Page'>
-      <h2 className='flex items-center bg-red-500'>
+      <h2 className='flex items-center'>
         <Link to={`/profile/${appState.user.username}`} className='mr-2'>
           <img className='w-8 h-8 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' />
         </Link>
-        <span className='ml-1'>Adamu</span>
+        <Link to={`/profile/${appState.user.username}`}>Adamu</Link>
       </h2>
 
-      <div className='profile-nav nav nav-tabs pt-2 mb-4'>
-        <a href='#' className='active nav-item nav-link'>
-          Posts: 3
-        </a>
-        <a href='#' className='nav-item nav-link'>
-          Followers: 101
-        </a>
-        <a href='#' className='nav-item nav-link'>
-          Following: 40
-        </a>
-      </div>
-
+      <ul class='flex border-b mt-5'>
+        <li class='-mb-px mr-1'>
+          <a class='bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold' href='#'>
+            Active
+          </a>
+        </li>
+        <li class='mr-1'>
+          <a class='bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold' href='#'>
+            Tab
+          </a>
+        </li>
+        <li class='mr-1'>
+          <a class='bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold' href='#'>
+            Tab
+          </a>
+        </li>
+        <li class='mr-1'>
+          <a class='bg-white inline-block py-2 px-4 text-gray-400 font-semibold' href='#'>
+            Tab
+          </a>
+        </li>
+      </ul>
       <div className='list-group'>
-        <a href='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #1</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </a>
-        <a href='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #2</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </a>
-        <a href='#' className='list-group-item list-group-item-action'>
-          <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #3</strong>
-          <span className='text-muted small'>on 2/10/2020 </span>
-        </a>
+        <Link to='#' className='list-group-item list-group-item-action'>
+          <img className='w-8 h-8 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' />
+          <strong>Example Post #1</strong>
+  <span className='text-muted small'>{" "}on 2/10/2020 </span>
+        </Link>
       </div>
     </Page>
   );
