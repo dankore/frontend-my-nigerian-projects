452fba17a6004551bb56ee156a66b6a81bb166b7
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-14 11:50:07 -0500

    update

--- app/components/Bid.js
+++ app/components/Bid.js
@@ -7,7 +7,7 @@ function Bid(props) {
   const bid = props.bid;
   const date = new Date(bid.createdDate);
   const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-  const descriptionFormatted = `${bid.description.split(' ').slice(0, 5).join(" ")}...`
+  const descriptionFormatted = `${bid.description.split(' ').slice(0, 5).join(' ')}...`;
 
   return (
     <tr className='border'>
@@ -25,7 +25,7 @@ function Bid(props) {
         </div>
       </td>
       <td className='px-6 py-4 whitespace-no-wrap border-b border-gray-200'>
-        <div className='text-sm leading-5 text-gray-900'>Project Title</div>
+        <div className='text-sm leading-5 text-gray-900'>{bid.title}</div>
         <div className='text-sm leading-5 text-gray-500'>
           <ReactMarkdown source={descriptionFormatted} allowedTypes={['paragraph', 'strong', 'emphasis', 'text', 'heading', 'list', 'listItem', 'link', 'linkReference']} />
         </div>
