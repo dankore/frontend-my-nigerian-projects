2ca1dfe65b23538ec249d524316c1796e2c4e64f
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-15 00:59:46 -0500

    update

--- app/pages/EditBidPage.js
+++ app/pages/EditBidPage.js
@@ -1,11 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useContext } from 'react';
 import Page from '../components/Page';
 import LoadingDotsIcon from '../components/LoadingDotsIcon';
 import { useParams, Link } from 'react-router-dom';
 import Axios from 'axios';
 import { useImmerReducer } from 'use-immer';
+import StateContext from '../StateContext';
+import DispatchContext from '../DispatchContext';
 
 function ViewSingleBid() {
+  const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
+
   const originalState = {
     title: {
       value: '',
@@ -45,6 +50,24 @@ function ViewSingleBid() {
           draft.sendCount++;
         }
         return;
+      case 'saveRequestStarted':
+        draft.isSaving = true;
+        return;
+      case 'saveRequestFinished':
+        draft.isSaving = false;
+        return;
+      case 'titleRules':
+        if (!action.value.trim()) {
+          draft.title.hasErrors = true;
+          draft.title.message = 'Title cannot be empty.';
+        }
+        return;
+      case 'descriptionRules':
+        if (!action.value.trim()) {
+          draft.description.hasErrors = true;
+          draft.description.message = 'Description cannot be empty.';
+        }
+        return;
     }
   }
 
@@ -68,28 +91,66 @@ function ViewSingleBid() {
     };
   }, []);
 
+  useEffect(() => {
+    if (state.sendCount) {
+      dispatch({ type: 'saveRequestStarted' });
+      const request = Axios.CancelToken.source();
+      (async function fetchBid() {
+        try {
+          const response = await Axios.post(
+            `/bid/${state.id}/edit`,
+            {
+              title: state.title.value,
+              description: state.description.value,
+              token: appState.user.token,
+            },
+            {
+              cancelToken: request.token,
+            }
+          );
+          dispatch({ type: 'saveRequestFinished' });
+          appDispatch({ type: 'flashMessage', value: 'Bid updated successfully.' });
+        } catch (error) {
+          console.log('Problem with fetching bids.');
+        }
+      })();
+      // IF COMPONENT IS UNMOUNTED, CANCEL AXIOS REQUEST
+      return () => {
+        request.cancel();
+      };
+    }
+  }, [state.sendCount]);
+
+  function submitEditBidForm(e) {
+    e.preventDefault();
+    dispatch({ type: 'submitRequest' });
+  }
+
   if (state.isFetching) {
     return <LoadingDotsIcon />;
   }
 
   return (
     <Page title='Edit Bid'>
-      <form>
-        <div className='mb-4'>
+      <form onSubmit={submitEditBidForm}>
+        <div className='relative mb-4'>
           <label htmlFor='title' className='w-full text-xs font-bold block mb-1 uppercase tracking-wide text-gray-700 '>
             Title
           </label>
-          <input onChange={e => dispatch({ type: 'titleChange', value: e.target.value })} value={state.title.value} id='title' autoFocus type='text' autoComplete='off' className='w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight' />
+          <input onBlur={e => dispatch({ type: 'titleRules', value: e.target.value })} onChange={e => dispatch({ type: 'titleChange', value: e.target.value })} value={state.title.value} id='title' autoFocus type='text' autoComplete='off' className='w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight' />
+          {state.title.hasErrors && <div className='text-xs text-red-600 liveValidateMessage'>{state.title.message}</div>}
         </div>
-
-        <div className=''>
+        <div className='relative'>
           <label htmlFor='bid-description' className='w-full text-xs font-bold block mb-1 uppercase tracking-wide text-gray-700 '>
             Description
           </label>
-          <textarea onChange={e => dispatch({ type: 'descriptionChange', value: e.target.value })} value={state.description.value} rows='6' name='body' id='bid-description' className='w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight' type='text' />
+          <textarea onBlur={e => dispatch({ type: 'descriptionRules', value: e.target.value })} onChange={e => dispatch({ type: 'descriptionChange', value: e.target.value })} value={state.description.value} rows='6' name='body' id='bid-description' className='w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight' type='text' />
+          {state.description.hasErrors && <div className='text-xs text-red-600 liveValidateMessage'>{state.description.message}</div>}
         </div>
 
-        <button className='w-full text-white rounded border border-white bg-blue-600 hover:bg-blue-800 px-2 py-3'>Save Updates</button>
+        <button disabled={state.isSaving} className='w-full text-white rounded border border-white bg-blue-600 hover:bg-blue-800 px-2 py-3'>
+          {state.isSaving ? 'Saving...' : 'Save Updates'}
+        </button>
       </form>
     </Page>
   );
