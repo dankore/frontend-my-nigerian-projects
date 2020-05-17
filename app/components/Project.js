da8c72df2161cbc9befc2b7f9a07f2fc935b2770
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-17 01:15:47 -0500

    Updates

--- app/components/Project.js
+++ app/components/Project.js
@@ -10,13 +10,13 @@ function Project(props) {
 
   return (
     <Link to={`/project/${project._id}`} className=''>
-      <div className='border border-gray-200 flex justify-between py-3 hover:bg-gray-100'>
-        <div className='px-6'>
+      <div className='border border-gray-200 flex py-3 hover:bg-gray-100'>
+        <div className='px-2 bg-green-400'>
           <div className='flex items-center'>
-            <div className='flex-shrink-0 h-10 w-10'>
+            <div className=''>
               <img className='h-10 w-10 rounded-full' src='https://gravatar.com/avatar/f69127052821e90dabb8c6cabd227e90?s=128' alt='' />
             </div>
-            <div className='ml-4'>
+            <div className='ml-2'>
               <div className='text-sm leading-5 font-medium text-gray-900'>
                 {project.author.firstName} {project.author.lastName}
               </div>
@@ -30,10 +30,10 @@ function Project(props) {
             <ReactMarkdown source={formatTitleAndDescription(project.description)} allowedTypes={['paragraph', 'image', 'strong', 'emphasis', 'text', 'heading', 'list', 'listItem', 'link', 'linkReference']} />
           </div>
         </div>
-        <div className='px-6'>
+        <div className='px-2'>
           <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>Active</span>
         </div>
-        <div className='px-6 text-sm leading-5 text-gray-500'>????</div>
+        <div className='text-sm leading-5 text-gray-500'>????</div>
       </div>
     </Link>
   );
