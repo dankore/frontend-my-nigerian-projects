b7460380401ca3a47e654d0cd54ad3d5cfe06c0f
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-17 21:49:25 -0500

    Updates

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -1,134 +1,62 @@
-import React, { useEffect } from "react"
-import Page from "../components/Page";
+import React, { useEffect, useContext } from 'react';
+import Page from '../components/Page';
+import { useImmer } from 'use-immer';
+import StateContext from '../StateContext';
+import LoadingDotsIcon from '../components/LoadingDotsIcon';
+import Axios from 'axios';
+import { Link } from 'react-router-dom';
+import DispatchContext from '../DispatchContext';
+import ReactMarkdown from 'react-markdown';
+import Project from '../components/Project';
 
 function Home() {
+  const appState = useContext(StateContext);
+  const appDispatch = useContext(DispatchContext);
+  const [state, setState] = useImmer({
+    isLoading: true,
+    feed: [],
+  });
+
+  useEffect(() => {
+    // IF COMPONENT IS UNMOUNTED, CANCEL AXIOS REQUEST
+    const request = Axios.CancelToken.source();
+
+    (async function fetchDataByUsername() {
+      try {
+        const response = await Axios.post('/getHomeFeed', { token: appState.user.token }, { CancelToken: request.token });
+        setState(draft => {
+          draft.isLoading = false;
+          draft.feed = response.data;
+        });
+      } catch (error) {
+        appDispatch({ type: 'flashMessageError', value: 'Fetching username failed.' });
+      }
+    })();
+    // CANCEL REQUEST
+    return () => {
+      request.cancel();
+    };
+  }, []);
+
+  if (state.isLoading) {
+    <LoadingDotsIcon />;
+  }
+
   return (
-    <Page title="Home" wide={true}>
-      <div className="flex flex-col">
-        <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-          <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
-            <table className="min-w-full">
-              <thead>
-                <tr>
-                  <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                  <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</th>
-                  <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                  <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Role</th>
-                  <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
-                </tr>
-              </thead>
-              <tbody className="bg-white">
-                <tr>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 h-10 w-10">
-                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                      </div>
-                      <div className="ml-4">
-                        <div className="text-sm leading-5 font-medium text-gray-900">Bernard Lane</div>
-                        <div className="text-sm leading-5 text-gray-500">bernardlane@example.com</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div className="text-sm leading-5 text-gray-900">Director</div>
-                    <div className="text-sm leading-5 text-gray-500">Human Resources</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-                  <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 h-10 w-10">
-                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1532910404247-7ee9488d7292?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                      </div>
-                      <div className="ml-4">
-                        <div className="text-sm leading-5 font-medium text-gray-900">Bernard Lane</div>
-                        <div className="text-sm leading-5 text-gray-500">bernardlane@example.com</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div className="text-sm leading-5 text-gray-900">Director</div>
-                    <div className="text-sm leading-5 text-gray-500">Human Resources</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-                  <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 h-10 w-10">
-                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1505503693641-1926193e8d57?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                      </div>
-                      <div className="ml-4">
-                        <div className="text-sm leading-5 font-medium text-gray-900">Bernard Lane</div>
-                        <div className="text-sm leading-5 text-gray-500">bernardlane@example.com</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <div className="text-sm leading-5 text-gray-900">Director</div>
-                    <div className="text-sm leading-5 text-gray-500">Human Resources</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-                  <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr>
-                  <td className="px-6 py-4 whitespace-no-wrap">
-                    <div className="flex items-center">
-                      <div className="flex-shrink-0 h-10 w-10">
-                        <img className="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                      </div>
-                      <div className="ml-4">
-                        <div className="text-sm leading-5 font-medium text-gray-900">Bernard Lane</div>
-                        <div className="text-sm leading-5 text-gray-500">bernardlane@example.com</div>
-                      </div>
-                    </div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap">
-                    <div className="text-sm leading-5 text-gray-900">Director</div>
-                    <div className="text-sm leading-5 text-gray-500">Human Resources</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap">
-                    <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">Inactive</span>
-                  </td>
-                  <td className="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-500">Owner</td>
-                  <td className="px-6 py-4 whitespace-no-wrap text-right text-sm leading-5 font-medium">
-                    <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+    <Page title='Home' wide={true}>
+      {state.feed.length > 0 && (
+        <>
+          <h2 className='text-center'>Latest from those you follow</h2>
+          <div>
+            {state.feed.map(project => {
+              return <Project project={project} key={project._id} />;
+            })}
           </div>
-        </div>
-      </div>
+        </>
+      )}
+      {state.feed.length == 0 && <h2>Feed is empty</h2>}
     </Page>
   );
 }
 
-export default Home
+export default Home;
