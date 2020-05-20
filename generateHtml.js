bda3ae2205de05188778947d36ffc2b5e6f7e943
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-20 09:52:26 -0500

    Updates

--- generateHtml.js
+++ generateHtml.js
@@ -34,8 +34,8 @@ function html(x) {
         integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous">
       </script>
     </head>
-    <body>
-      <div id="app">
+    <body style='display: relative'>
+      <div id="app" style='display: relative'>
       ${x}
       </div>
     </body>
