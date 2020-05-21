4687b4e36e192e4d055943da09faf36dd3ffab1f
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-20 19:30:34 -0500

    Updates

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import HeaderLoggedOut from './HeaderLoggedOut';
 import HeaderLoggedIn from './HeaderLoggedIn';
@@ -6,25 +6,13 @@ import StateContext from '../StateContext';
 
 function Header() {
   const appState = useContext(StateContext);
-  const [isOpen, setIsOpen] = useState(false);
-
-  // TOGGLE MENU
-  function handleOpenAndCloseMenu() {
-    setIsOpen(prev => {
-      if (prev == false) {
-        return (prev = true);
-      } else {
-        return (prev = false);
-      }
-    });
-  }
 
   return (
     <header className='bg-blue-600'>
-      <nav className='flex items-center justify-between flex-wrap lg:max-w-2xl lg:mx-auto'>
+      <nav className={`flex items-center justify-between flex-wrap lg:max-w-2xl lg:mx-auto ${appState.loggedIn ? ' ' : 'py-1'}`}>
         <Link to='/' className='mx-auto lg:mx-0 block flex items-center text-white'>
           <i className='fas fa-home'></i>
-          <span className='ml-2 font-semibold text-xl tracking-tight'>Bid for my Projects</span>
+          <span className='hidden lg:block ml-2 font-semibold text-xl tracking-tight'>Bid for my Projects</span>
         </Link>
 
         <div className='flex items-center justify-between mx-auto'>
