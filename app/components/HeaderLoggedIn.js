eca69ba228d18dfb3c962e0a798d1d132cbd2ea4
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-20 19:37:12 -0500

    Updates

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,20 +1,8 @@
-import React, { useContext } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import DispatchContext from '../DispatchContext';
-import StateContext from '../StateContext';
-import ReactToolTip from 'react-tooltip';
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 import ProfileImageLoggedIn from './ProfileImageLoggedIn';
 
 function HeaderLoggedIn(props) {
-  const appDispatch = useContext(DispatchContext);
-  const appState = useContext(StateContext);
-
-  function handleLogout() {
-    appDispatch({ type: 'logout' });
-    props.history.push('/');
-    appDispatch({ type: 'flashMessage', value: 'Logged Out Successfully.' });
-  }
-
   return (
     <div className='lg:flex lg:justify-center lg:items-center'>
       <ProfileImageLoggedIn />
