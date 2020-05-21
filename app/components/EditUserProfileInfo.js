62dc5ed337f48435b599cf726171b937156c21d5
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-21 13:08:15 -0500

    edit profile updates

--- app/components/EditUserProfileInfo.js
+++ app/components/EditUserProfileInfo.js
@@ -4,12 +4,27 @@ import Page from '../components/Page';
 import Axios from 'axios';
 import { useImmerReducer } from 'use-immer';
 import DispatchContext from '../DispatchContext';
+import StateContext from '../StateContext';
 import { CSSTransition } from 'react-transition-group';
+import LoadingDotsIcon from './LoadingDotsIcon';
 
 function RegistrationPage(props) {
   const appDispatch = useContext(DispatchContext);
+  const appState = useContext(StateContext);
 
   const initialState = {
+    profileData: {
+      profileUsername: '...',
+      profileFirstName: '',
+      profileLastName: '',
+      profileAvatar: 'https://gravatar.com/avatar/palceholder?s=128',
+      isFollowing: false,
+      counts: {
+        projectCount: '',
+        followerCount: '',
+        followingCount: '',
+      },
+    },
     username: {
       value: '',
       hasErrors: false,
@@ -27,12 +42,15 @@ function RegistrationPage(props) {
       hasErrors: false,
       message: '',
     },
-
+    isLoading: true,
     submitCount: 0,
   };
 
   function Reducer(draft, action) {
     switch (action.type) {
+      case 'fetchDataComplete':
+        draft.profileData = action.value;
+        return;
       // USERNAME
       case 'usernameImmediately':
         draft.username.hasErrors = false;
@@ -85,6 +103,9 @@ function RegistrationPage(props) {
           draft.lastName.message = 'Last name field cannot be empty.';
         }
         return;
+      case 'isLoadingFinished':
+        draft.isLoading = false;
+        return;
       // SUBMIT
       case 'submitForm':
         if (!draft.username.hasErrors && draft.username.isUnique && !draft.firstName.hasErrors && !draft.lastName.hasErrors && !draft.email.hasErrors && draft.email.isUnique && !draft.password.hasErrors) {
@@ -122,6 +143,26 @@ function RegistrationPage(props) {
       };
     }
   }, [state.username.checkCount]);
+
+  // FETCH USER INFO
+  useEffect(() => {
+    // IF COMPONENT IS UNMOUNTED, CANCEL AXIOS REQUEST
+    const request = Axios.CancelToken.source();
+
+    (async function fetchData() {
+      try {
+        const response = await Axios.post(`/profile/${appState.user.username}`, { token: appState.user.token }, { CancelToken: request.token });
+        dispatch({ type: 'fetchDataComplete', value: response.data });
+        dispatch({ type: 'isLoadingFinished' });
+      } catch (error) {
+        appDispatch({ type: 'flashMessageError', value: 'Fetching username failed.' });
+      }
+    })();
+    // CANCEL REQUEST
+    return () => {
+      request.cancel();
+    };
+  }, []);
 
   // SUBMIT FORM
   useEffect(() => {
@@ -167,6 +208,10 @@ function RegistrationPage(props) {
     dispatch({ type: 'submitForm' });
   }
 
+  if (state.isLoading) {
+    return <LoadingDotsIcon />;
+  }
+
   // CSS
   const CSSTransitionStyle = { color: '#e53e3e', fontSize: 0.75 + 'em' };
 
@@ -179,35 +224,38 @@ function RegistrationPage(props) {
               <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1' htmlFor='username'>
                 Username <span className='text-red-600'>*</span>
               </label>
-              <input onChange={e => dispatch({ type: 'usernameImmediately', value: e.target.value })} id='username' autoComplete='off' spellCheck='false' className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='username' type='text' />
+              <input value={state.profileData.profileUsername} onChange={e => dispatch({ type: 'usernameImmediately', value: e.target.value })} id='username' autoComplete='off' spellCheck='false' className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='username' type='text' />
               <CSSTransition in={state.username.hasErrors} timeout={330} className='liveValidateMessage' unmountOnExit>
                 <div style={CSSTransitionStyle} className='liveValidateMessage'>
                   {state.username.message}
                 </div>
               </CSSTransition>
             </div>
+
             <div className='relative w-full px-3 mb-3'>
               <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1' htmlFor='first-name'>
                 First Name <span className='text-red-600'>*</span>
               </label>
-              <input onChange={e => dispatch({ type: 'firstnameImmediately', value: e.target.value })} id='first-name' autoComplete='off' spellCheck='false' className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='first-name' type='text' />
+              <input value={state.profileData.profileFirstName} onChange={e => dispatch({ type: 'firstnameImmediately', value: e.target.value })} id='first-name' autoComplete='off' spellCheck='false' className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white' id='first-name' type='text' />
               <CSSTransition in={state.firstName.hasErrors} timeout={330} className='liveValidateMessage' unmountOnExit>
                 <div style={CSSTransitionStyle} className='liveValidateMessage'>
                   {state.firstName.message}
                 </div>
               </CSSTransition>
             </div>
+
             <div className='relative w-full px-3 mb-3'>
               <label className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-1' htmlFor='last-name'>
                 Last Name <span className='text-red-600'>*</span>
               </label>
-              <input onChange={e => dispatch({ type: 'lastnameImmediately', value: e.target.value })} id='last-name' autoComplete='off' className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500' id='last-name' type='text' />
+              <input value={state.profileData.profileLastName} onChange={e => dispatch({ type: 'lastnameImmediately', value: e.target.value })} id='last-name' autoComplete='off' className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500' id='last-name' type='text' />
               <CSSTransition in={state.lastName.hasErrors} timeout={330} className='liveValidateMessage' unmountOnExit>
                 <div style={CSSTransitionStyle} className='liveValidateMessage'>
                   {state.lastName.message}
                 </div>
               </CSSTransition>
             </div>
+
             <button className='rounded w-full bg-blue-600 hover:bg-blue-800 text-white m-3 p-3'>Save Changes</button>
           </div>
         </form>
