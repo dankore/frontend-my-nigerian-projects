1ba08e955ba325f425f2c7f0a2421c8de0f6f2ce
Adamu M. Dankore <zimmazone@yahoo.com>
2020-05-20 20:11:21 -0500

    Updates

--- app/components/Header.js
+++ app/components/Header.js
@@ -9,7 +9,7 @@ function Header() {
 
   return (
     <header className='bg-blue-600'>
-      <nav className={`flex items-center justify-between flex-wrap lg:max-w-2xl lg:mx-auto ${appState.loggedIn ? ' ' : 'py-1'}`}>
+      <nav className={`flex items-center justify-between flex-wrap lg:max-w-2xl lg:mx-auto ${appState && appState.loggedIn ? ' ' : 'py-1'}`}>
         <Link to='/' className='mx-auto lg:mx-0 block flex items-center text-white'>
           <i className='fas fa-home'></i>
           <span className='hidden lg:block ml-2 font-semibold text-xl tracking-tight'>Bid for my Projects</span>
