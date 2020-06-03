11820de83c2ad4b2a288424d9e65e7f0983e640f
Adamu M. Dankore <zimmazone@yahoo.com>
2020-06-03 08:02:15 -0500

    Update files

--- app/components/Project.js
+++ app/components/Project.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 
@@ -6,6 +6,7 @@ function Project(props) {
   const project = props.project;
   const date = new Date(project.createdDate);
   const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  // TRUNCATE TITLE AND DESCRIPTION
   const formatTitleAndDescription = s => {
     const inputToArray = s.split(' ');
     if (inputToArray.length < 6) {
@@ -18,8 +19,19 @@ function Project(props) {
   function timeRemainingInDays() {
     const bidSubmissionDeadline = new Date(project.bidSubmissionDeadline);
     const todaysDate = new Date();
-    const timeDifferenceInSecs = Math.abs(todaysDate - bidSubmissionDeadline);
-    return /** TIME DIFFERENCE IN DAYS */ Math.ceil(timeDifferenceInSecs / (24 * 60 * 60 * 1000));
+    // const timeDifferenceInSecs = Math.abs(todaysDate - bidSubmissionDeadline);
+    const timeDifferenceInSecs = bidSubmissionDeadline - todaysDate;
+
+    let days /** TIME DIFFERENCE IN DAYS */ = Math.ceil(timeDifferenceInSecs / (24 * 60 * 60 * 1000));
+    //CONDITIONS
+    if (days == 0) {
+      return <span className='bg-yellow-300 p-1 rounded'>Hurry, bid submission ends today!</span>;
+    }
+    if (days < 0) {
+      return <span className='bg-red-300 p-1 rounded'>Bid submission is closed.</span>;
+    }
+
+    return <span className='bg-green-300 p-1 rounded'>{`${days} days left to bid`}</span>;
   }
 
   return (
@@ -44,9 +56,7 @@ function Project(props) {
             <ReactMarkdown source={formatTitleAndDescription(project.description)} allowedTypes={['paragraph', 'image', 'strong', 'emphasis', 'text', 'heading', 'list', 'listItem', 'link', 'linkReference']} />
           </div>
         </div>
-        <div className='ml-6'>
-          <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>{timeRemainingInDays()} days left to bid</span>
-        </div>
+        <div className='ml-6 px-2 text-xs leading-5 font-semibold rounded-full'>{timeRemainingInDays()}</div>
       </div>
     </Link>
   );
