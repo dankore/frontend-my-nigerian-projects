db569720a5c5fd97bc0f9bef7d01b5e1a1d9c361
Adamu M. Dankore <zimmazone@yahoo.com>
2020-06-23 17:29:22 -0500

    sign in refac

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -4,12 +4,15 @@ import { Link } from 'react-router-dom';
 function HeaderLoggedOut() {
   return (
     <div className='flex justify-center items-center'>
-      <Link className='block mr-4 px-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-600 hover:bg-green-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out' to='/register'>
+      <Link className='block mr-5 px-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-green-600 hover:bg-green-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out' to='/register'>
         <i className='fas fa-plus mr-1'></i>
         Register
       </Link>
-      <Link className='block hover:text-gray-400' to='/login'>
-        Login <i className='fas fa-sign-in-alt'></i>
+      <Link className='flex items-center hover:text-gray-400' to='/login'>
+        <span className='text-3xl'>
+          <i className='far fa-user-circle'></i>
+        </span>
+        <span className='mx-1'>Login</span>
       </Link>
     </div>
   );
