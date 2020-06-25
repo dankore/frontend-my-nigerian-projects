76a1eef1d4d2eb97088cf508b2216c6a8750eecb
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-06-25 09:11:35 +0000

    Update file

--- app/pages/AccountRecoveryEnterPassword.js
+++ app/pages/AccountRecoveryEnterPassword.js
@@ -4,21 +4,80 @@ import { useParams, withRouter } from 'react-router-dom';
 import { useImmerReducer } from 'use-immer';
 import Axios from 'axios';
 import DispatchContext from '../DispatchContext';
+import { CSSTransition } from 'react-transition-group';
+import { CSSTransitionStyle } from '../helpers/CSSHelpers';
 
 function AccountRecoveryEnterPassword(props) {
     const appDispatch = useContext(DispatchContext);
     const initialState = {
+        password: {
+            value: '',
+            hasErrors: false,
+            message: ''
+        },
+         reEnterPassword: {
+            value: '',
+            hasErrors: false,
+            message: ''
+        },
         passwordResetToken: useParams().token
     }
 
-    function reducer(){
+    function reducer(draft, action){
         switch(action.type){
-            case 'fetchComplete':
+            case 'passwordImmediately':
+                draft.password.hasErrors = false;
+                draft.password.value = action.value;
+
+                 if (draft.password.value.trim() == '') {
+                    draft.password.hasErrors = true;
+                    draft.password.message = 'Password field is empty.';
+                }
+
+                if (draft.password.value.length > 50) {
+                    draft.password.hasErrors = true;
+                    draft.password.message = 'Password must cannot be more than 50 characters.';
+                }
                 return;
+            case 'passwordAfterDelay':
+                if (draft.password.value.length < 6) {
+                    draft.password.hasErrors = true;
+                    draft.password.message = 'Password must be at least 6 characters.';
+                }
+                return;
+             case 'reEnterPasswordImmediately':
+                draft.reEnterPassword.hasErrors = false;
+                draft.reEnterPassword.value = action.value;
+
+                if (draft.reEnterPassword.value.trim() == '') {
+                    draft.reEnterPassword.hasErrors = true;
+                    draft.reEnterPassword.message = 'Password field is empty.';
+                }
+                return;
+            case 'reEnterPasswordAfterDelay':
+               if(draft.password.value != draft.reEnterPassword.value){
+                   draft.reEnterPassword.hasErrors = true;
+                   draft.reEnterPassword.message = 'Password do not match.';
+               }
+                return;
+            
         }
     }
 
     const [state, dispatch] = useImmerReducer(reducer, initialState);
+
+    function handleSubmit(e){
+        e.preventDefault();
+
+    }
+
+    // PASSWORD AFTER DELAY
+    useEffect(()=>{
+        if(state.password.value){
+            const delay = setTimeout(() => dispatch({type: "passwordAfterDelay"}), 800);
+            return () => clearTimeout(delay);
+        }
+    }, [state.password.value])
 
 
     useEffect(()=>{
@@ -27,7 +86,9 @@ function AccountRecoveryEnterPassword(props) {
             try {
                 const response = await Axios.post('/choose-new-password', { passwordResetToken: state.passwordResetToken }, { cancelToken: request.token })
                 if(response.data == "Success"){
+                    // DO NOTHING?
                     console.log("valid token");
+                  
                 } else {
                     props.history.push("/reset-password");
                     appDispatch({type: 'flashMessageError', value: "Password reset token is invalid or has expired. Please generate another token below."})
@@ -50,44 +111,37 @@ function AccountRecoveryEnterPassword(props) {
         </div>
         <p className='text-xl font-semibold text-center leading-tight mb-8 mt-3'>Choose a New Password</p>
         <div className="w-full max-w-md sm:max-w-sm mx-auto mt-6 mx-4">
-        <form
-            action="/reset-password/<%= token %>"
-            method="POST"
-            className="bg-white border rounded px-4 pt-4 pb-8 m-4"
-        >
-            <div className="mb-4">
-            <p className="text-xl mb-4">Step 2 of 2:</p>
-            <label
-                className="block uppercase text-gray-700 text-sm font-bold mb-2"
-                htmlFor="new-password"
-            >
-                New Password
-            </label>
-            <input
-                name="new_password"
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="new-password"
-                type="password"
-            />
+         <form onSubmit={handleSubmit} className='p-3 sm:p-4 border rounded'>
+           <div className='relative mb-4'>
+            <div className='flex justify-between items-center mb-1 text-xs uppercase font-bold tracking-wide text-gray-700'>
+              <label htmlFor='password'>
+                New Password{' '}
+              </label>
             </div>
-            <div className="mb-1">
-            <label
-                className="block uppercase text-gray-700 text-sm font-bold mb-2"
-                htmlFor="confirm-new-password"
-            >
-                Confirm new Password
-            </label>
-            <input
-                name="confirm_new_password"
-                className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                id="confirm-new-password"
-                type="password"
-            />
+            <input onChange={e => dispatch({ type: 'passwordImmediately', value: e.target.value })} id='password' type='password' className='w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight' />
+            <CSSTransition in={state.password.hasErrors} timeout={330} classNames='liveValidateMessage' unmountOnExit>
+              <div style={CSSTransitionStyle} className='liveValidateMessage'>
+                {state.password.message}
+              </div>
+            </CSSTransition>
+          </div>
+          <div className='relative mb-4'>
+            <div className='flex justify-between items-center mb-1 text-xs uppercase font-bold tracking-wide text-gray-700'>
+              <label htmlFor='re-enter-password'>
+                Re-enter New Password{' '}
+              </label>
             </div>
+            <input onChange={e => dispatch({ type: 'reEnterPasswordImmediately', value: e.target.value })} id='re-enter-password' type='password' className='w-full py-3 px-4 appearance-none bg-gray-200 focus:outline-none focus:border-gray-500 focus:bg-white appearance-none border rounded py-1 px-3 text-gray-700 leading-tight' />
+            <CSSTransition in={state.reEnterPassword.hasErrors} timeout={330} classNames='liveValidateMessage' unmountOnExit>
+              <div style={CSSTransitionStyle} className='liveValidateMessage'>
+                {state.reEnterPassword.message}
+              </div>
+            </CSSTransition>
+          </div>
             <p className="text-red-600 text-xs mb-6 italic">
             New password should be a minimun of 6 characters
             </p>
-           <button type='submit' className='relative w-full  mt-3 inline-flex items-center justify-center px-4 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out'>
+           <button type='submit' className='relative w-full inline-flex items-center justify-center px-4 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out'>
               <span className='absolute left-0 inset-y-0 flex items-center pl-3'>
                 <svg className='h-5 w-5 text-blue-500  transition ease-in-out duration-150' fill='currentColor' viewBox='0 0 20 20'>
                   <path fillRule='evenodd' d='M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z' clipRule='evenodd' />
