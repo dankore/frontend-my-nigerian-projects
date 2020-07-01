aef5610a05704ef3743e737cff3692b23a619380
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-01 07:09:12 +0000

    update file

--- app/pages/CreateBid.js
+++ app/pages/CreateBid.js
@@ -117,6 +117,10 @@ function CreateBid(props) {
           draft.phone.hasErrors = true;
           draft.phone.message = 'Phone cannot be empty';
         }
+        if(/[^\d]/.test(action.value.trim())){
+             draft.phone.hasErrors = true;
+             draft.phone.message = 'Phone must be only numbers.';
+        }
         return;
       case 'emailUpdate':
         draft.email.hasErrors = false;
