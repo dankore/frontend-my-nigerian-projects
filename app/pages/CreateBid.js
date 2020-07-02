025d389a898c564493517921bf5e066056930332
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-02 09:37:21 -0500

    Enhancement

--- app/pages/CreateBid.js
+++ app/pages/CreateBid.js
@@ -267,20 +267,19 @@ function CreateBid(props) {
 
   return (
     <Page margin='mx-2' wide={true} title='Create Bid'>
+      <div className='flex justify-center text-blue-600'>
+        <svg className='w-12' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+          <line x1='12' y1='5' x2='12' y2='19'></line>
+          <line x1='5' y1='12' x2='19' y2='12'></line>
+        </svg>
+      </div>
+      <p className='text-xl font-semibold text-center leading-tight'>Create New Bid For:</p>
+      <h2 className='text-center px-2 mb-8 text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-3xl sm:leading-9'>
+        <Link to={`/project/${state.projectId}`} className='underline hover:text-blue-600'>
+          {state.project.title}
+        </Link>
+      </h2>
       <form onSubmit={handleSubmitBid}>
-        <div className='flex justify-center text-blue-600'>
-          <svg className='w-12' xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
-            <line x1='12' y1='5' x2='12' y2='19'></line>
-            <line x1='5' y1='12' x2='19' y2='12'></line>
-          </svg>
-        </div>
-        <p className='text-xl font-semibold text-center leading-tight mb-2 mt-3'>Create New Bid For:</p>
-        <h2 className='text-center px-2 mb-8 text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-3xl sm:leading-9'>
-          <Link to={`/project/${state.projectId}`} className='underline hover:text-blue-600'>
-            {state.project.title}
-          </Link>
-        </h2>
-
         <div className='border border-gray-200 bg-white p-3 shadow-sm lg:rounded-lg'>
           {/* WHAT BEST DESCRIBES YOU */}
           <div className='mb-4'>
