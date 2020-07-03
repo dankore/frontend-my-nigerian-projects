1ed0fb1796dc32603514e37a735a07111f682390
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-03 08:41:17 -0500

    Update file

--- app/pages/AboutPage.js
+++ app/pages/AboutPage.js
@@ -14,6 +14,50 @@ function About() {
             <p className='text-base leading-6 text-blue-600 font-semibold tracking-wide uppercase'>Welcome</p>
             <h3 className='mt-2 max-w-3xl text-2xl leading-8 font-semibold font-display text-gray-900 sm:text-3xl sm:leading-9 lg:max-w-4xl lg:text-4xl lg:leading-10 lg:mx-auto lg:text-center'>Get paid helping me with my projects in Nigeria</h3>
           </div>
+
+          <div className='mt-8 space-y-6 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-8 lg:mt-12'>
+            <div className='flex items-center'>
+              <div className='flex-shrink-0'>
+                <span className='h-10 w-10 bg-blue-500 flex items-center justify-center rounded-full text-lg leading-10 font-display font-bold text-white'>01</span>
+              </div>
+              <div className='ml-4'>
+                <p className='text-lg leading-6 font-medium text-gray-900'>I post a project</p>
+              </div>
+            </div>
+            <div className='flex items-center'>
+              <div className='flex-shrink-0'>
+                <span className='h-10 w-10 bg-blue-500 flex items-center justify-center rounded-full text-lg leading-10 font-display font-bold text-white'>02</span>
+              </div>
+              <div className='ml-4'>
+                <p className='text-lg leading-6 font-medium text-gray-900'>You bid on the project</p>
+              </div>
+            </div>
+            <div className='flex items-center'>
+              <div className='flex-shrink-0'>
+                <span className='h-10 w-10 bg-blue-500 flex items-center justify-center rounded-full text-lg leading-10 font-display font-bold text-white'>03</span>
+              </div>
+              <div className='ml-4'>
+                <p className='text-lg leading-6 font-medium text-gray-900'>I pick a winner</p>
+              </div>
+            </div>
+            <div className='flex items-center'>
+              <div className='flex-shrink-0'>
+                <span className='h-10 w-10 bg-blue-500 flex items-center justify-center rounded-full text-lg leading-10 font-display font-bold text-white'>04</span>
+              </div>
+              <div className='ml-4'>
+                <p className='text-lg leading-6 font-medium text-gray-900'>You make money</p>
+              </div>
+            </div>
+            <div className='flex items-center'>
+              <div className='flex-shrink-0'>
+                <span className='h-10 w-10 bg-blue-500 flex items-center justify-center rounded-full text-lg leading-10 font-display font-bold text-white'>05</span>
+              </div>
+              <div className='ml-4'>
+                <p className='text-lg leading-6 font-medium text-gray-900'> I save money </p>
+              </div>
+            </div>
+          </div>
+
           <div className='my-12 flex justify-center'>
             {!appState.loggedIn && (
               <div className='inline-flex rounded'>
