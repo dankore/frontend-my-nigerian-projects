70cbac9044def77c285edcdb57893f1c35dbc5a8
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-02 23:51:43 -0500

    suggestions

---
+++ app/components/Suggestions.js
@@ -0,0 +1,7 @@
+import React, { useEffect } from 'react';
+
+function Suggestions() {
+  return <div className='lg:w-3/4 bg-red-500 lg:ml-16 lg:rounded-lg mt-10'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque voluptas cumque fugiat in voluptates aliquid. Reprehenderit ullam nisi dignissimos quaerat accusantium voluptas obcaecati dolorum perspiciatis dolore itaque. Aliquam, facilis at? Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur officiis nobis nostrum numquam esse corrupti commodi atque doloribus nesciunt laborum laudantium incidunt quas ipsa, totam vero voluptate ipsum. Tenetur, vero? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem dolor doloribus, aut voluptas earum ipsum, repudiandae sed eligendi corporis temporibus sint, illum tenetur excepturi aliquid porro dolores facere distinctio aperiam.</div>;
+}
+
+export default Suggestions;
