bb395037498aeb1717bf38a1c99193c9f38db75e
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-03 19:22:53 -0500

    Update file

--- app/components/SidebarLeft.js
+++ app/components/SidebarLeft.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 function SidebarLeft() {
   return (
     <div className='hidden lg:block max-w-2xl mx-auto lg:w-3/4 lg:ml-16 lg:rounded-lg mt-10'>
-      <h2 className='shadow-sm border-b lg:rounded-t-lg bg-white text-2xl leading-8 font-semibold font-display text-gray-900 sm:leading-9 lg:leading-10 lg:mx-auto pl-3 py-2'>Quick Links</h2>
-      <Link className='shadow-sm p-3 bg-white flex items-center border-b hover:bg-gray-100' to='/how-to-bid'>
+      <h2 className=' lg:rounded-t-lg text-2xl leading-8 font-semibold font-display text-gray-900 sm:leading-9 lg:leading-10 lg:mx-auto pl-3 py-2'>Quick Links</h2>
+      <Link className=' p-3 flex items-center hover:bg-gray-100' to='/how-to-bid'>
         <i className='fas fa-file-contract mr-1 text-gray-600'></i>
         How To Bid
       </Link>
-      <Link className='shadow-sm p-3 bg-white text-green-600 flex items-center border-b hover:bg-gray-100' to='/create-project'>
+      <Link className=' p-3 text-green-600 flex items-center  hover:bg-gray-100' to='/create-project'>
         <i className='fas fa-plus mr-1'></i>
         Create Project
       </Link>
-      <Link className='shadow-sm p-3 bg-white flex items-center border-b hover:bg-gray-100' to='/reset-password'>
+      <Link className=' p-3 flex items-center hover:bg-gray-100' to='/reset-password'>
         <i className='fas fa-unlock-alt mr-1 text-gray-600'></i>
         Reset Password
       </Link>
--- app/components/SidebarRight.js
+++ app/components/SidebarRight.js
@@ -8,10 +8,10 @@ function SidebarRight({ projects }) {
 
   return (
     <div className='max-w-2xl mx-auto lg:w-3/4 lg:ml-16 lg:rounded-lg mt-10'>
-      <h2 className='shadow-sm border-b lg:rounded-t-lg bg-white text-2xl leading-8 font-semibold font-display text-gray-900 sm:leading-9 lg:leading-10 lg:mx-auto pl-3 py-2'>Who to follow</h2>
+      <h2 className='  lg:rounded-t-lg  text-2xl leading-8 font-semibold font-display text-gray-900 sm:leading-9 lg:leading-10 lg:mx-auto pl-3 py-2'>Who to follow</h2>
       {projects?.map((project, index) => {
         return (
-          <Link to={`/profile/${project.author.username}`} key={index} className='shadow-sm p-3 bg-white flex items-center border-b hover:bg-gray-100'>
+          <Link to={`/profile/${project.author.username}`} key={index} className=' p-3  flex items-center  hover:bg-gray-100'>
             <img className='h-12 w-12 rounded-full' src={project.author.avatar} alt='ProfilePic' />
             <div className='ml-3'>
               <div className='flex items-center'>
