51b3a4f6825cd85a8052acf16817ec16b7ecd452
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-04 09:42:25 -0500

    bug fix

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='text-center mt-10'>
       <div>
-        <Link to='/'>Home</Link> | <Link to='/how-to-bid'>How to Bid</Link> | <Link to='/about'>About</Link> | <Link to='/terms'>Terms</Link> | <Link to='/privacy'>Privacy</Link> | <Link to='https://github.com/dankore'>GitHub</Link>
+        <Link to='/'>Home</Link> | <Link to='/how-to-bid'>How to Bid</Link> | <Link to='/about'>About</Link> | <Link to='/terms'>Terms</Link> | <Link to='/privacy'>Privacy</Link> | <a href='https://github.com/dankore'>GitHub</a>
       </div>
       Copyright &copy; {new Date().getFullYear()} Adamu M. Dankore. All rights reserved.
     </div>
