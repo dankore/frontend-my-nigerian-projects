8a7bcbd3df0e7545ddf30704b3df774ae3c9eccc
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-08 10:17:26 +0000

    Upload image

--- app/pages/ViewSingleProject.js
+++ app/pages/ViewSingleProject.js
@@ -80,7 +80,7 @@ function ViewSingleProject(props) {
   return (
     <Page margin='mx-2' title={project.title}>
       <div className='shadow-md my-6'>
-        <div className='bg-white rounded-t-lg p-3 mb-6 flex justify-between items-center'>
+        <div className='bg-white rounded-t-lg p-3 flex justify-between items-center'>
           <div className=''>
             <h2 className='mr-3 text-2xl leading-8 font-semibold tracking-tight font-display text-gray-900 sm:text-3xl sm:leading-9'>
               {appState.loggedIn ? (project.author.firstName == appState.user.firstName ? 'Your' : project.author.firstName + "'s") : project.author.firstName + "'s"} project: {project.title}
@@ -114,7 +114,12 @@ function ViewSingleProject(props) {
             </span>
           )}
         </div>
-        <p className='pl-2 text-lg leading-7 font-medium tracking-tight text-gray-900'>Description:</p>
+
+          {/* IMAGE  */}
+          {project.image && (<img className='object-contain bg-white h-48 w-full' src={`${project.image}`} alt='projectImage'/>)}
+       
+        {/* DESCRIPTION */}
+        <p className='pl-2 mt-6 text-lg leading-7 font-medium tracking-tight text-gray-900'>Description:</p>
         <div style={{ overflowWrap: 'anywhere', minWidth: 0 + 'px' }} className='p-2 bg-white'>
           <ReactMarkdown source={project.description} allowedTypes={['paragraph', 'image', 'strong', 'emphasis', 'text', 'heading', 'list', 'listItem', 'link', 'linkReference']} />
         </div>
