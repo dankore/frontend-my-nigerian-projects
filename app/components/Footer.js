6d91a0e1dc59b5bfb5b3b8d6fce59accf1ddb607
Adamu Muhammad Dankore <zimmazone@yahoo.com>
2020-07-09 02:19:13 +0000

    Modal set up

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div className='text-center mt-10'>
+        <div class="modal-overlay"></div>
       <div>
         <Link to='/'>Home</Link> | <Link to='/how-to-bid'>How to Bid</Link> | <Link to='/about'>About</Link> | <Link to='/terms'>Terms</Link> | <Link to='/privacy'>Privacy</Link> | <Link to='/cookies'>Cookies</Link> | <a href='https://github.com/dankore/frontend-my-nigerian-projects'>GitHub</a>
       </div>
--- app/pages/ProfilePage.js
+++ app/pages/ProfilePage.js
@@ -166,8 +166,14 @@ function ProfilePage(props) {
           </NavLink>
         </ul>
       </div>
+      {/* MODAL CHANGE PROFILE IMAGE */}
+      <div className='modal'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+          cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
 
-      {/* PAGE */}
+      {/* PAGES */}
 
       <Switch>
         <Route exact path='/profile/:username'>
