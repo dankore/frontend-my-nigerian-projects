b8a4b5743943b6c16fd9829bc1ffab15bfd3a5e4
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-10 09:27:27 -0500

    Update file

--- app/pages/ProfilePage.js
+++ app/pages/ProfilePage.js
@@ -151,6 +151,7 @@ function ProfilePage(props) {
             });
 
             appDispatch({ type: 'updateAvatar', value: image_url });
+            appDispatch({ type: 'toggleUpdateProfileImage' });
           }
         })();
 
