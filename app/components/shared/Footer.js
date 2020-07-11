e85aac9bc7f9b7a67272991757c00acd151c1424
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-11 14:17:02 -0500

    improvements

--- app/components/shared/Footer.js
+++ app/components/shared/Footer.js
@@ -10,7 +10,16 @@ function Footer() {
     <div className='text-center mt-10'>
       {/* MODAL OVERLAY */}
       {appState && appState.toggleOptionsProfileImage && <div onClick={() => appDispatch({ type: 'toggleOptionsProfileImage' })} className='modal-overlay absolute cursor-pointer'></div>}
-      {appState && appState.toggleImageViewer && <div onClick={() => appDispatch({ type: 'toggleImageViewer' })} className='modal-overlay bg-black absolute cursor-pointer'><button className='rounded-full shadow-lg text-lg left-0 p-4 bg-white absolute'>X</button></div>}
+      {appState && appState.toggleImageViewer && (
+        <div onClick={() => appDispatch({ type: 'toggleImageViewer' })} className='modal-overlay bg-black absolute cursor-pointer'>
+          <div className='absolute flex items-center left-0 shadow-lg mt-2 text-3xl'>
+            <button className='rounded-full px-3 bg-white'>X</button>
+            <a href='/'>
+              <i className='fas fa-home text-white ml-2'></i>
+            </a>
+          </div>
+        </div>
+      )}
       {appState && appState.toggleUpdateProfileImage && <div onClick={() => appDispatch({ type: 'toggleUpdateProfileImage' })} style={{ backgroundColor: '#0000008c' }} className='modal-overlay bg-opacity-50 absolute cursor-pointer'></div>}
       <div>
         <Link to='/'>Home</Link> | <Link to='/how-to-bid'>How to Bid</Link> | <Link to='/about'>About</Link> | <Link to='/terms'>Terms</Link> | <Link to='/privacy'>Privacy</Link> | <Link to='/cookies'>Cookies</Link> | <a href='https://github.com/dankore/frontend-my-nigerian-projects'>GitHub</a>
