0a1a6dad874f411079266fe5d57e078a1c6cd790
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-13 06:56:15 -0500

    welcome message css

--- app/components/shared/Welcome.js
+++ app/components/shared/Welcome.js
@@ -2,8 +2,8 @@ import React, { useEffect } from 'react';
 
 function Welcome() {
   return (
-    <div className='bg-green-600 text-white absolute bottom-0 text-lg p-2 lg:rounded-lg'>
-      &#129306; Thank you for visiting My Nigerian Projects Dot Com.
+    <div className='flex justify-center bg-red-500 w-full'>
+      <div className='bg-green-600 text-white absolute max-w-lg text-lg p-2 lg:rounded-lg'>&#129306; Thank you for visiting My Nigerian Projects Dot Com.</div>
     </div>
   );
 }
