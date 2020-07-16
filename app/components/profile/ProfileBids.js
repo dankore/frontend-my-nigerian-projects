ebcf22fc5fe7c677eb6262c926b59e85bd2fbce9
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-16 11:36:43 -0500

    profile bids

--- app/components/profile/ProfileBids.js
+++ app/components/profile/ProfileBids.js
@@ -2,15 +2,15 @@ import React, { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import LoadingDotsIcon from '../shared/LoadingDotsIcon';
 import Axios from 'axios';
-import Project from '../projects/Project';
 import StateContext from '../../StateContext';
 import { useImmer } from 'use-immer';
 import ReactPaginate from 'react-paginate';
+import { Link } from 'react-router-dom';
 
 function ProfileBids() {
   const appState = useContext(StateContext);
   const { username } = useParams();
-  const [projects, setProjects] = useImmer({
+  const [bids, setBids] = useImmer({
     isLoading: true,
     feed: [],
     offset: 0,
@@ -19,16 +19,18 @@ function ProfileBids() {
     currentPage: 0,
   });
 
+  console.log({ bids: bids.feed });
+
   //PAGINATION STARTS
   // GET CURRENT PROJECT
-  const current_paginated_projects = projects.feed.slice(projects.offset, projects.offset + projects.perPage);
+  const current_paginated_bids = bids.feed.slice(bids.offset, bids.offset + bids.perPage);
 
   // CHANGE PAGE
   function handleProjectsPagination(e) {
     const selectedPage = e.selected;
-    const offset = selectedPage * projects.perPage;
+    const offset = selectedPage * bids.perPage;
 
-    setProjects(draft => {
+    setBids(draft => {
       draft.currentPage = selectedPage;
       draft.offset = offset;
     });
@@ -43,22 +45,24 @@ function ProfileBids() {
         const response = await Axios.get(`/profile/${username}/bids`, {
           cancelToken: request.token,
         });
-        setProjects(draft => {
+        setBids(draft => {
           draft.isLoading = false;
         });
+
+        console.log(response.data);
+
         if (response.data) {
-          setProjects(draft => {
+          setBids(draft => {
             draft.feed = response.data;
-            draft.pageCount = Math.ceil(response.data.length / projects.perPage);
+            draft.pageCount = Math.ceil(response.data.length / bids.perPage);
           });
         }
       } catch (error) {
-        console.log('Problem with fetching bids.');
+        console.log(error.message);
       }
     })();
     // IF COMPONENT IS UNMOUNTED, CANCEL AXIOS REQUEST
     return () => request.cancel();
-    
   }, [username]);
 
   function showThisWhenNoProject() {
@@ -73,19 +77,43 @@ function ProfileBids() {
     }
   }
 
-  if (projects.isLoading) {
+  function bidItemsTotal(array) {
+    return array.reduce((total, currentElem) => {
+      const currentTotal = +currentElem.quantity * +currentElem.price_per_item;
+      return total + currentTotal;
+    }, 0);
+  }
+
+  if (bids.isLoading) {
     return <LoadingDotsIcon />;
   }
 
   return (
     <div>
-      {projects.feed.length > 0 ? (
+      {bids.feed.length > 0 ? (
         <>
-          {current_paginated_projects.map(project => {
-            return <Project project={project} key={project._id} />;
+          {current_paginated_bids.map((bid, index) => {
+            return (
+              <Link key={index} to={`/${bid.projectId}/bid/${bid.id}`} style={{ minHeight: 60 + 'px' }} className='flex flex-wrap shadow lg:rounded-lg border border-gray-300 bg-white hover:bg-gray-100 p-2'>
+                <div className='flex items-center text-sm leading-5 mr-6'>
+                  <i className='text-gray-700 fas fa-id-badge'></i>
+                  <p className='ml-1.5'>{bid.whatBestDescribesYou}</p>
+                </div>
+
+                <div className='flex items-center text-sm leading-5 mr-6'>
+                  <i className='text-gray-700 fas fa-money-bill-wave'></i>
+                  <p className='ml-1.5'>{new Intl.NumberFormat().format(bidItemsTotal(bid.items))}</p>
+                </div>
+
+                <div className='flex items-center text-sm leading-5'>
+                  <i className='text-gray-700 fas fa-user-cog'></i>
+                  <p className='ml-1.5'>{bid.yearsOfExperience > 1 ? `${bid.yearsOfExperience} years` : `${bid.yearsOfExperience} year`} of experience </p>
+                </div>
+              </Link>
+            );
           })}
           {/* ONLY SHOW PAGINATION IF THERE'S MORE  THAN NUMBER OF PROJECTS PER PAGE */}
-          {projects.feed.length > projects.perPage && <ReactPaginate previousLabel={'prev'} nextLabel={'next'} breakLabel={'...'} breakClassName={'break-me'} pageCount={projects.pageCount} marginPagesDisplayed={2} pageRangeDisplayed={5} onPageChange={handleProjectsPagination} containerClassName={'pagination'} subContainerClassName={'pages pagination'} activeClassName={'active'} />}
+          {bids.feed.length > bids.perPage && <ReactPaginate previousLabel={'prev'} nextLabel={'next'} breakLabel={'...'} breakClassName={'break-me'} pageCount={bids.pageCount} marginPagesDisplayed={2} pageRangeDisplayed={5} onPageChange={handleProjectsPagination} containerClassName={'pagination'} subContainerClassName={'pages pagination'} activeClassName={'active'} />}
         </>
       ) : (
         <p className='p-3 shadow lg:rounded-lg bg-white'>{showThisWhenNoProject()}</p>
