6c7591e35b8315e285e12c1bec3b5153f064402a
Adamu M. Dankore <zimmazone@yahoo.com>
2020-07-19 10:29:43 -0500

    update file

--- generateHtml.js
+++ generateHtml.js
@@ -44,10 +44,10 @@ function html(x) {
 }
 
 /*
-  We can use ReactDomServer (you can see how we imported
+  I can use ReactDomServer (you can see how I imported
   that at the very top of this file) to generate a string
-  of HTML text. We simply give it a React component and
-  here we are using the JSX syntax.
+  of HTML text. I simply give it a React component and
+  here I are using the JSX syntax.
 */
 const reactHtml = ReactDOMServer.renderToString(<Shell />);
 
@@ -55,18 +55,18 @@ const reactHtml = ReactDOMServer.renderToString(<Shell />);
   Call our "html" function which has the skeleton or
   boilerplate HTML, and give it the string that React
   generated for us. Our "html" function will insert
-  the React string inside the #app div. So now we will
-  have a variable in memory with the exact string we
-  want, we just need to save it to a file.
+  the React string inside the #app div. So now I will
+  have a variable in memory with the exact string I
+  want, I just need to save it to a file.
 
 */
 const overallHtmlString = html(reactHtml);
 
 /*
-  This course is not about Node, but here we are simply
+  Here I are simply
   saving our generated string into a file named
   index-template.html. Please note that this Node task
-  will fail if the directory we told it to live within
+  will fail if the directory I told it to live within
   ("app" in this case) does not already exist.
 */
 const fileName = './app/index-template.html';
