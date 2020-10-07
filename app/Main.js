c038ba7eb9f0524032d2b7aee54a479d93f57bca
Adamu Muhammad Dankore <45286534+dankore@users.noreply.github.com>
2020-10-06 19:30:48 -0500

    add  be api

--- app/Main.js
+++ app/Main.js
@@ -10,8 +10,7 @@ import DispatchContext from './DispatchContext';
 // STATE MANAGEMENT ENDS
 
 // AXIOS COMMON URL
-console.log(process.env.BACKENDURL);
-Axios.defaults.baseURL = 'https://backend-my-nigerian-projects.herokuapp.com';
+Axios.defaults.baseURL =  'https://backend-my-nigerian-projects-omb4q.ondigitalocean.app'; //'https://backend-my-nigerian-projects.herokuapp.com';
 
 // COMPONENTS
 import Header from './components/shared/Header';
